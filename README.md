c26f6e515f4ed9a43bb826bcce3d1687abc7693b
Nate Flood <nate@famconnect.net>
2016-07-14 09:14:39 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Discourse modifications for ECHOcommunity Navigation
+
+## What it does:
+ - Loads Minified Bootstrap Javascript
+ - Loads namespace customized Bootstrap
+ - Loads CSS customizations
---
+++ plugin.rb
@@ -0,0 +1,19 @@
+# name: ECHO Navigation
+# about: UI Modifications to bring in the ECHO nav bar system tro the top of the forums
+# version: 0.0.1
+# authors: Nate Flood for ECHO Inc
+
+# require 'pry'
+# javascript
+register_asset "javascripts/vendor/bootstrap.js"
+
+# stylesheet
+register_asset "stylesheets/echo-nav.css"
+
+after_initialize do
+	ApplicationController.class_eval do
+    def set_layout
+        File.expand_path('../app/views/layouts/echo.html.erb', __FILE__)
+    end
+  end
+end
