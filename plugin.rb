46781187b9e2b0ca8084466cbb279f9603d84a52
Nate Flood <nate@famconnect.net>
2019-01-15 22:20:09 -0500

    Plugin now uses standard method to insert header and footer. Better for future upgrades.

--- plugin.rb
+++ plugin.rb
@@ -1,19 +1,39 @@
 # name: ECHO Navigation
 # about: UI Modifications to bring in the ECHO nav bar system to the top of the forums
-# version: 1.8.12
+# version: 2.0.0
 # authors: Nate Flood for ECHO Inc
 
-# require 'pry'
 # javascript
 register_asset "javascripts/vendor/bootstrap.js"
 
 # stylesheet
 register_asset "stylesheets/echo-nav.css"
 
-after_initialize do
-	ApplicationController.class_eval do
-    def set_layout
-    	use_crawler_layout? ? 'crawler' : File.expand_path('../app/views/layouts/echo.html.erb', __FILE__)
-    end
-  end
+# custom html
+register_html_builder('server:header') do |controller|
+	Rails.cache.fetch("header_#{I18n.locale}", expires_in: 1.day) do
+		newheader = "<div class='echocommunity_core'>"
+		begin
+			newheader += open("https://www.echocommunity.org/#{I18n.locale}/remote/header", :read_timeout => 10).read
+			newheader += open("https://www.echocommunity.org/#{I18n.locale}/remote/navigation", :read_timeout => 10).read
+			newheader += open("https://www.echocommunity.org/#{I18n.locale}/remote/language", :read_timeout => 10).read
+		rescue OpenURI::HTTPError => e
+			newheader += "An Error Occurred: " + e.message
+		end
+		newheader += "</div>"
+		newheader
+	end
+end
+
+register_html_builder('server:before-body-close') do |controller|
+	Rails.cache.fetch("footer_#{I18n.locale}", expires_in: 1.day) do
+		newfooter = "<div class='echocommunity_core'><div class='container-fluid'>"
+		begin
+			newfooter += open("https://www.echocommunity.org/#{I18n.locale}/remote/footer", :read_timeout => 10).read
+		rescue OpenURI::HTTPError => e
+			newfooter += "An Error Occurred: " + e.message
+		end
+		newfooter += "</div></div>"
+		newfooter
+	end
 end
